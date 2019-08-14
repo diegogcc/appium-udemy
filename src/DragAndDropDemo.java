04083e7973fafc526e5f336702bdfe4eb29bfb4c
Diego Campo <diegocampo@ZCOBTMH100478-DiegoCampo.local>
2019-08-14 18:11:05 -0500

    Drag and Drop gesture added

---
+++ src/DragAndDropDemo.java
@@ -0,0 +1,29 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidTouchAction;
+
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+
+public class DragAndDropDemo extends Capabilities{
+
+	public static void main(String[] args) throws MalformedURLException {
+		AndroidDriver<AndroidElement> androidDriver = capabilities();
+		androidDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		androidDriver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+		androidDriver.findElementByAndroidUIAutomator("text(\"Drag and Drop\")").click();;
+		
+		AndroidTouchAction t = new AndroidTouchAction(androidDriver);
+		WebElement dot1 = androidDriver.findElementById("io.appium.android.apis:id/drag_dot_1");
+		WebElement dot3 = androidDriver.findElementById("io.appium.android.apis:id/drag_dot_3");
+		t.longPress(longPressOptions().withElement(element(dot1))).moveTo(element(dot3)).release().perform();
+		t.longPress(element(dot3)).moveTo(element(dot1)).release().perform(); //Other option
+		
+	}
+
+}

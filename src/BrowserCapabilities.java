18a3d12ac711562a36847fcb582f30cf552d45a7
Diego Campo <diegocampo@ZCOBTMH100478-DiegoCampo.local>
2019-08-18 20:23:30 -0500

    Automation for browsers demo including scroll with JS

---
+++ src/BrowserCapabilities.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class BrowserCapabilities {
+
+	public static AndroidDriver<AndroidElement> capabilities(String device) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, device);
+//		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device"); // Any real device connected
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		cap.setCapability("browserName", "chrome");
+		
+				
+		AndroidDriver<AndroidElement> androidDriver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub") ,cap);
+		return androidDriver;
+//		AndroidUIAutomator
+//		1. $ANDROID_HOME/tools/bin/uiautomatorviewer to run a scan of the emulator
+	}
+	
+}
---
+++ src/BrowserDemo.java
@@ -0,0 +1,32 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class BrowserDemo extends BrowserCapabilities{
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		AndroidDriver<AndroidElement> androidDriver = capabilities("Pixel_2_API_25");
+		androidDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		androidDriver.get("http://facebook.com");
+//		androidDriver.findElementByXPath("//*[@id=\"m_login_email\"]").sendKeys("holo-mail");
+//		androidDriver.findElementByXPath("//*[@id=\"m_login_password\"]").sendKeys("psssss");
+//		androidDriver.findElementByXPath("//*[@id=\"login_form\"]/div[4]/div/div[2]/div/div/div[2]").click();
+		
+		androidDriver.get("http://m.cricbuzz.com");
+		androidDriver.findElementByXPath("//*[@id=\"top\"]/div/div[3]/div[2]/a").click();
+		androidDriver.findElementByXPath("//*[@id=\"top\"]/div/div[16]/table/tbody/tr[1]/td[1]/a").click();
+		System.out.println(androidDriver.getCurrentUrl());
+//		Steps to scroll using JS
+		JavascriptExecutor jse = (JavascriptExecutor) androidDriver;
+		jse.executeScript("window.scrollBy(0,480)", "");
+		
+		System.out.println(androidDriver.findElementByXPath("//*[@id=\"top\"]/div/div[10]/h4").getAttribute("class").contains("header"));
+		
+	}
+
+}

60cd7e6900f46fa7d348d2501a7951bcadf3d875
Diego Campo <diegocampo@ZCOBTMH100478-DiegoCampo.local>
2019-08-22 11:57:58 -0500

    Webview - Native context switch

---
+++ src/ECommerceTC5.java
@@ -0,0 +1,54 @@
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidTouchAction;
+
+	/*
+	 * TC5:
+	 * Validate if mobile gestures are working for links and navigate to a webview
+	 */
+public class ECommerceTC5 extends Capabilities{
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		// TODO Auto-generated method stub
+		AndroidDriver<AndroidElement> androidDriver = capabilities("Pixel_2_API_25");
+		androidDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		String prod = "Air Jordan 4 Retro";
+		ECommerceTC1.enterTheShop(androidDriver);
+		ECommerceTC2.addToCart(androidDriver, prod);
+		ECommerceTC3.verifyCheckoutPage(androidDriver, prod);
+		seeTermsOfConditions(androidDriver);
+		closeTermsOfConditions(androidDriver);
+		tapMarketingCheckbox(androidDriver);
+	}
+
+	public static void tapMarketingCheckbox(AndroidDriver<AndroidElement> driver) {
+		// TODO Auto-generated method stub
+		AndroidTouchAction t = new AndroidTouchAction(driver);
+		WebElement checkbox = driver.findElementByClassName("android.widget.CheckBox");
+		t.tap(tapOptions().withElement(element(checkbox))).perform();
+	}
+
+	public static void closeTermsOfConditions(AndroidDriver<AndroidElement> driver) throws InterruptedException {
+		// TODO Auto-generated method stub
+		Thread.sleep(2000); //Sleep added so TC modal is visible
+		driver.findElementByXPath("//android.widget.Button[@text='CLOSE']").click();
+	}
+
+	public static void seeTermsOfConditions(AndroidDriver<AndroidElement> driver) {
+		// TODO Auto-generated method stub
+		AndroidTouchAction t = new AndroidTouchAction(driver);
+		WebElement tc = driver.findElementById("com.androidsample.generalstore:id/termsButton");
+		t.longPress(longPressOptions().withElement(element(tc)).withDuration(ofSeconds(3))).release().perform();
+	}
+
+}
---
+++ src/ECommerceTC6.java
@@ -0,0 +1,55 @@
+import java.net.MalformedURLException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+	/*
+	 * TC6:
+	 * Verify if user can go to the webview and back to the native app
+	 */
+public class ECommerceTC6 extends Capabilities{
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		// TODO Auto-generated method stub
+		AndroidDriver<AndroidElement> androidDriver = capabilities("Pixel_2_API_25");
+		androidDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		String prod = "Air Jordan 4 Retro";
+		goToWebView(androidDriver, prod);
+		goBackToNativeApp(androidDriver);
+	}
+
+	public static void goBackToNativeApp(AndroidDriver<AndroidElement> driver) {
+		// TODO Auto-generated method stub
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+		Set<String> contextNames = driver.getContextHandles();
+		for(String contextName : contextNames) {
+			System.out.println(contextName);
+		}
+		driver.context((String) contextNames.toArray()[0]);
+		System.out.println("Switching to:" + (String)contextNames.toArray()[0]);
+	}
+
+	public static void goToWebView(AndroidDriver<AndroidElement> driver, String prod) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ECommerceTC1.enterTheShop(driver);
+		ECommerceTC2.addToCart(driver, prod);
+		ECommerceTC3.verifyCheckoutPage(driver, prod);
+		driver.findElementById("com.androidsample.generalstore:id/btnProceed").click();
+		Thread.sleep(2000);
+		System.out.println("Web View Contexts:");
+		Set<String> contextNames = driver.getContextHandles();
+		for(String contextName : contextNames) {
+			System.out.println(contextName);
+		}
+		driver.context((String) contextNames.toArray()[1]);
+		driver.findElementByName("q").sendKeys("Success ");
+		driver.findElementByName("q").sendKeys(Keys.ENTER);		
+	}
+
+}
